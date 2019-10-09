5ff8481b9dce554780d86f535aa7883e05efd4fb
PIngMeLive <55232298+pingmelive@users.noreply.github.com>
2019-10-09 18:16:46 +0530

    Update Pingmelive.php

--- Pingmelive.php
+++ Pingmelive.php
@@ -1,43 +1,5 @@
 <?php
 /**
- * CodeIgniter
- *
- * An open source application development framework for PHP
- *
- * This content is released under the MIT License (MIT)
- *
- * Copyright (c) 2014 - 2019, British Columbia Institute of Technology
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package	CodeIgniter
- * @author	EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
- * @license	https://opensource.org/licenses/MIT	MIT License
- * @link	https://codeigniter.com
- * @since	Version 1.0.0
- * @filesource
- */
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-/**
  * CodeIgniter PingMeLive Class
  *
  * 	This is a library which helps you to get LIVE notifications of actions taking place on your webistes and applications.
@@ -46,12 +8,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	Easy right!.
  *
  * @package		PingMeLive-CodeIgniter Integration
- * @subpackage	Libraries
- * @category	Libraries
+ * @subpackage		Libraries
+ * @category		Libraries
  * @author		PingMeLive Team
  * @link		https://pingmelive.com/
- * @lastModified		08-OCT, 2019 2:05 PM
+ * @lastModified	08-OCT, 2019 2:05 PM
  */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
 class CI_PingMeLive {
 public function __construct()
    {
@@ -105,21 +70,28 @@ public function simpleEvent($groupTitle, $messageText)
         $this->sendData($groupTitle, $messageText);
         return true;
     }
-public function startErrorPings()
-    {
-        set_error_handler(function($errno, $errstr, $errfile, $errline)
-        {
-            $this->pingError($errno, $errstr, $errfile, $errline);
-            return true;
-        }, E_ALL);
-        register_shutdown_function(function()
-        {
-            $last_error = error_get_last();
-            if ($last_error['type'] === E_ERROR) {
-                $this->pingError(E_ERROR, $last_error['message'], $last_error['file'], $last_error['line']);
-            }
-            return true;
-        });
+public function startErrorPings(){	
+	error_reporting(E_ALL);
+	ini_set('display_errors', 'off');
+	set_error_handler(function($errno, $errstr, $errfile, $errline)
+	{
+	$this->pingError($errno, $errstr, $errfile, $errline);
+	return true;
+	}, E_ALL);
+	set_exception_handler(function($exception){
+	$this->pingError($exception->getCode(),'Exception: '.$exception->getMessage(), $exception->getFile(), $exception->getLine());
+	return false;
+	set_status_header(500);
+	exit(1);
+	});
+	register_shutdown_function(function(){
+	$last_error = error_get_last();
+	if (isset($last_error) &&
+	($last_error['type'] & (E_ERROR | E_PARSE | E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_COMPILE_WARNING)))
+	{
+	$this->pingError($last_error['type'], $last_error['message'], $last_error['file'], $last_error['line']);
+	}
+	});
         return true;
     }
 public function detailedEvent($groupTitle, $messageText, $detailedText)
